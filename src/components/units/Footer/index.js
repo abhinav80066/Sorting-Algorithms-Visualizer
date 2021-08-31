9affbf5b8087839beab23dcc8eb8d1df780504c2
abhinav80066 <abhinav.chaudhary800@gmail.com>
2021-08-31 20:45:35 +0530

    Added the Components for AppControls, Menu, ProgressBar and Footer

---
+++ src/components/units/Footer/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      <section>
+        Designed and built with{' '}
+        <span className="Footer__Heart">&hearts;</span> by{' '}
+        <a
+          href="https://www.linkedin.com/in/abhinav-chaudhary-04b857192/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Abhinav Chaudhary
+        </a>
+      </section>
+
+      <section className="Footer__Items">
+        {/* <a
+          href="https://youtu.be/JFjvVmvC3pQ"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Demo
+        </a> */}
+        <a
+          href="https://github.com/abhinav80066/Sorting-Algorithms-Visualizer"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Code
+        </a>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
